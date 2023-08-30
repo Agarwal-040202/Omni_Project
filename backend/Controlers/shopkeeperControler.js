a385dc6005b8a92a3ea5281d12e05e84382287be
Agarwal-040202 <104666014+Agarwal-040202@users.noreply.github.com>
2023-08-31 00:25:02 +0530

    backend commit

--- backend/Controlers/shopkeeperControler.js
+++ backend/Controlers/shopkeeperControler.js
@@ -93,3 +93,115 @@ export const getShopkeeperDetail = (req, res) => {
     });
 };
 
+
+// export const updateShopkeeperDetail = (req, res) => {
+//     let {
+//         Logged_User_ID,
+//         Logged_Email_ID,
+//         Logged_User_Role,
+//         Created_By,
+//         Updated_By,
+//         Is_Active,
+//         Created_At,
+//         Shopkeeper_ID,
+//         Firm_Name,
+//         Shopkeeper_First_Name,
+//         Shopkeeper_Last_Name,
+//         Contact,
+//         Whatsup_Contact,
+//         GST_Number,
+//         Shopkeeper_Email,
+//         Address1,
+//         Country,
+//         State,
+//         City,
+//         Pincode,
+//         Village_Street
+//     } = req.body;
+
+//     // CHECK IF SHOPKEEPER EXISTS
+//     const checkQuery = "SELECT * FROM shopkeeperdetails WHERE Shopkeeper_ID = ?";
+//     db.query(checkQuery, [Shopkeeper_ID], (err, data) => {
+//         if (err) {
+//             console.log(err);
+//             return res.status(500).json(err);
+//         }
+//         if (!data.length) {
+//             console.log("Shopkeeper does not exist!");
+//             return res.status(404).json("Shopkeeper does not exist!");
+//         }
+
+//         // UPDATE SHOPKEEPER DETAILS
+//         const updateQuery = "UPDATE shopkeeperdetails SET " +
+//             "`Logged_User_ID` = ?, " +
+//             "`Logged_Email_ID` = ?, " +
+//             "`Logged_User_Role` = ?, " +
+//             "`Created_By` = ?, " +
+//             "`Updated_By` = ?, " +
+//             "`Is_Active` = ?, " +
+//             "`Created_At` = ?, " +
+//             "`Firm_Name` = ?, " +
+//             "`Shopkeeper_First_Name` = ?, " +
+//             "`Shopkeeper_Last_Name` = ?, " +
+//             "`Contact` = ?, " +
+//             "`Whatsup_Contact` = ?, " +
+//             "`GST_Number` = ?, " +
+//             "`Shopkeeper_Email` = ?, " +
+//             "`Address1` = ?, " +
+//             "`Country` = ?, " +
+//             "`State` = ?, " +
+//             "`City` = ?, " +
+//             "`Pincode` = ?, " +
+//             "`Village_Street` = ? " +
+//             "WHERE `Shopkeeper_ID` = ?";
+
+//         const values = [
+//             Logged_User_ID,
+//             Logged_Email_ID,
+//             Logged_User_Role,
+//             Created_By,
+//             Updated_By,
+//             Is_Active,
+//             Created_At,
+//             Firm_Name,
+//             Shopkeeper_First_Name,
+//             Shopkeeper_Last_Name,
+//             Contact,
+//             Whatsup_Contact,
+//             GST_Number,
+//             Shopkeeper_Email,
+//             Address1,
+//             Country,
+//             State,
+//             City,
+//             Pincode,
+//             Village_Street,
+//             Shopkeeper_ID
+//         ];
+
+//         db.query(updateQuery, values, (err, result) => {
+//             if (err) {
+//                 console.log(err, values);
+//                 return res.status(500).json(err);
+//             }
+
+//             const updatedDataQuery = "SELECT * FROM shopkeeperdetails WHERE Shopkeeper_ID = ?";
+//             db.query(updatedDataQuery, [Shopkeeper_ID], (err, updatedData) => {
+//                 if (err) {
+//                     console.log(err);
+//                     return res.status(500).json(err);
+//                 }
+
+//                 console.log("Updated data:", updatedData[0]);
+
+//                 return res.status(200).json({
+//                     status: 200,
+//                     data: updatedData[0],
+//                     message: "Shopkeeper details have been updated successfully.",
+//                 });
+//             });
+//         });
+//     });
+// };
+
+
