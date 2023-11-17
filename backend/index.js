31c2b21fdb522e8d48d5a690316ce02f482030d4
Sumit Agarwal <singhalsumit3nov1988@gmail.com>
2023-11-18 03:44:31 +0530

    new code push backend 18-11-23 time 3.44

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,15 @@ var corsOptions = {
     origin: 'https://omniproject-production.up.railway.app',
     optionsSuccessStatus: 200 // For legacy browser support
 }
-
-app.use(cors(corsOptions))
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Access-Control-Allow-Headers, Content-Type, Authorization, Origin, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next();
+  });
+  
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.json())
